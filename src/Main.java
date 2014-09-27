b9cf0cb1c8ab87974614c7ab768031338c3a69a9
ivanfroman <ivanfroman@gmail.com>
2014-09-27 20:01:02 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ivan
+ * Date: 27/09/14
+ * Time: 19:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+
+
+    }
+
+}
